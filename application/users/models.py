c0a423b49a4bb4b2f665af621d43f6bf58dfa8a8
Ollie <olliebyrne2@outlook.com>
2023-09-25 15:07:30 +0100

    added email to user table

--- application/users/models.py
+++ application/users/models.py
@@ -5,17 +5,19 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(30), nullable=False)
     password = db.Column(db.String(30), nullable=False)  
     
     tokens = db.relationship('Token', back_populates='user', cascade='all, delete-orphan')
     comments = db.relationship('Comment', back_populates='user', cascade='all, delete-orphan')
 
     def __repr__(self):
-        return f"User(id: {self.id}, username: {self.username})"
+        return f"User(id: {self.id}, username: {self.username}, email: {self.email})"
     
     @property
     def json(self):
         return {
             "id": self.id,
             "username": self.username,
+            "email": self.email
         }
