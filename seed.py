f1a751070adede1800c85c39294d0939ade36996
Ollie <olliebyrne2@outlook.com>
2023-09-25 10:29:17 +0100

    created comments route and SQL table

--- seed.py
+++ seed.py
@@ -1,7 +1,6 @@
 from application import db
-# from application.users.models import User
-# from application.tokens.models import Token
 from application.recipes.models import Recipe
+from application.comments.models import Comment
 
 db.drop_all()
 print("Dropping database")
@@ -11,19 +10,24 @@ print("Creating database")
 print("Seeding database")
 
 
-entry1= Recipe(
+entry1 = Recipe(
     name="Spaghetti Carbonara",
     description="A creamy pasta infused with salt, pepper and pancetta"
 )
 
-entry2= Recipe(
+entry2 = Recipe(
     name="Spaghetti Bolognese",
-    description="Beef Mince Cooked in assorted chopes vehe"
+    description="Beef Mince Cooked in assorted chopped vegetables"
 )
 
-
 db.session.add_all([entry1, entry2])
+db.session.commit() 
 
 
+comment1 = Comment(
+    comment="This recipe is amazing!",
+    recipe_id=entry1.id 
+)
 
+db.session.add(comment1)
 db.session.commit()
